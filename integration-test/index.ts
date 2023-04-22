167efe37eea1ea9736b2a645c0375c18d6bebddc
Akash Shah <akashshah.shah3@gmail.com>
2023-04-22 19:25:41 -0400

    setup integration test env | added instructions in README

---
+++ integration-test/index.ts
@@ -0,0 +1,15 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+
+chai.use(chaiHttp);
+
+before(async function () {
+    // TODO: setup test env
+  }
+);
+
+// TODO: run tests
+
+after(async () => {
+  // TODO: release resources
+});
