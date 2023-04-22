50dbd4925090cb29ccddaf242f21451c69404c55
Akash Shah <akashshah.shah3@gmail.com>
2023-04-22 16:27:21 -0400

    implemented body parser

--- src/core/http/server.ts
+++ src/core/http/server.ts
@@ -1,6 +1,7 @@
+import bodyParser from "body-parser";
+import Cors from "@/core/http/express-middlewares/cors";
 import express from "express";
 import http from "http";
-import Cors from "@/core/http/express-middlewares/cors";
 import * as Winston from "../winston";
 
 const logger = Winston.getLogger(module.filename);
@@ -8,8 +9,14 @@ const logger = Winston.getLogger(module.filename);
 export const app = express();
 const server = http.createServer(app);
 
+const BODY_MAX_SIZE = "1mb";
+
 export const configure = (enableDevMode: boolean) => {
+  app.use(bodyParser.json({limit: BODY_MAX_SIZE}));
+  app.use(bodyParser.urlencoded({limit: BODY_MAX_SIZE, extended: true}));
+
   app.use(Cors(enableDevMode));
+
   app.set("trust proxy", 1);
 };
 
